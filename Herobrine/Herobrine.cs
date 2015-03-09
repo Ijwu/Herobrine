72bc9b763255940efd7be76729e25e2d29ba5e3e
Hussein <hfarran@umich.edu>
2015-03-09 19:57:04 -0400

    Initial commit with base command stuff down.

---
+++ Herobrine/Herobrine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Utils = TShockAPI.Utils;
+
+namespace Herobrine
+{
+    public class Herobrine : TerrariaPlugin
+    {
+        public override string Name
+        {
+            get { return "Herobrine"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(1,0,0); }
+        }
+
+        public override string Author
+        {
+            get { return "Iwju"; }
+        }
+
+        public override string Description
+        {
+            get { return "Allows you to haunt a player. Spoopy."; }
+        }
+
+        public Herobrine(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command("herobrine.haunt", HauntPlayer, "haunt"));
+        }
+
+        private void HauntPlayer(CommandArgs args)
+        {
+            if (args.Parameters.Count > 0)
+            {
+                var plr = args.Parameters[0];
+                var players = Utils.Instance.FindPlayer(plr);
+                if (players.Count != 1)
+                {
+                    if (players.Count > 1)
+                    {
+                        args.Player.SendErrorMessage("Found multiple players {0}.", string.Join(", ", players));
+                        return;
+                    }
+                    args.Player.SendErrorMessage("No player found by the name {0}.", plr);
+                    return;
+                }
+
+
+            }
+        }
+    }
+}
